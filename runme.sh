c5667046e610605e4ee3eff30c322334b89519e6
Jeremy Gwinnup <jeremy@gwinnup.org>
2016-07-15 12:31:38 -0400

    IP address for container

--- runme.sh
+++ runme.sh
@@ -3,15 +3,14 @@
 #settings
 THREADS=8
 
-
 # get docker container IP address
-
-IP=`awk 'NR==1 {print $1}' /etc/hosts`
+# last line in container /etc/hosts
+IP=`awk 'END{print}' /etc/hosts | awk '{print $1}' `
 echo $IP
 
 #start moses2 as a daemon
-bin/moses2 -f config/aren-noproc-server-optimized-moses2.ini --threads $THREADS 
+moses/bin/moses2 -f config/aren-noproc-server-optimized-moses2.ini --threads $THREADS 
 
 #Run REST-Moses adapter
-/usr/bin/python bin/python-server.py -ip $IP -mosesurl http://127.0.0.1:6003/RPC2 -moseshome moses -slang ar -tlang en -log moses.log
+/usr/bin/python bin/python-server.py -ip $IP -mosesurl http://127.0.0.1:6003/RPC2 -moseshome moses -slang ar -tlang en -log moses
 
