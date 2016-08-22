928abfd23421c51299f1827e867fdf395500ac17
Jeremy Gwinnup <jeremy@gwinnup.org>
2016-08-22 13:51:36 -0400

    Modified to allow cherrypy to have more threads for requests

--- runme.sh
+++ runme.sh
@@ -2,15 +2,19 @@
 
 #settings
 THREADS=8
+RESTTHREADS=100
 
 # get docker container IP address
 # last line in container /etc/hosts
-IP=`awk 'END{print}' /etc/hosts | awk '{print $1}' `
-echo $IP
+# IP=`awk 'END{print}' /etc/hosts | awk '{print $1}' `
+# echo $IP
+
+#global listen
+IP = 0.0.0.0
 
 #start moses2 as a daemon
 moses/bin/moses2 -f config/aren-noproc-server-optimized-moses2.ini --threads $THREADS 
 
 #Run REST-Moses adapter
-/usr/bin/python bin/python-server.py -ip $IP -mosesurl http://127.0.0.1:6003/RPC2 -moseshome moses -slang ar -tlang en -log moses
+/usr/bin/python bin/python-server.py -nthreads $RESTTHREADS -ip $IP -mosesurl http://127.0.0.1:6003/RPC2 -moseshome moses -slang ar -tlang en -log moses
 
